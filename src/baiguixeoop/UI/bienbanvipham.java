65d43884dfdf8c51f74248c84aa620737b2a005c
Nguyễn Khánh An <155174988+khanhanprovip@users.noreply.github.com>
2024-05-29 08:42:06 +0700

    Update bienbanvipham.java

--- src/baiguixeoop/UI/bienbanvipham.java
+++ src/baiguixeoop/UI/bienbanvipham.java
@@ -35,7 +35,7 @@ public class bienbanvipham extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         ss=new SystemService();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
         ngaygio.setText(LocalDateTime.now().format(formatter));
         kh.setText(vph.getKh().getTenKH());
         sdt.setText(vph.getKh().getSodienThoai());
